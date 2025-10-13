4598f9be34868245e5d13d433fff9a063d5c6c7f
TeodoraIK <ivanovaa.teodora@gmail.com>
2025-10-13 12:54:39 +0300

    Typescript- HomeWork

---
+++ TypeScript-HomeWork/index.ts
@@ -0,0 +1,12 @@
+let name: string = "Ivan Ivanov";
+let age: number = 30;
+console.log("Hello! I am", name , age , "years old");
+
+let num1: number = 5;
+let num2: number = 6;
+const addition = num1 + num2;
+console.log("Result is:",addition);
+
+const num: number = 25; 
+const isWithinRange: boolean = num >= 10 && num <= 50;
+console.log("Is", num, "within range 10 to 50?" ,isWithinRange);
