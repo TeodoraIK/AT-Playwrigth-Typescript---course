37887450342c287c1ac43029a181305e26f2bb75
TeodoraIK <ivanovaa.teodora@gmail.com>
2025-10-16 19:55:34 +0300

    Array methods

--- arrays.ts
+++ arrays.ts
@@ -24,8 +24,32 @@ else {
     return `Welcome ${element}`;
 }
 });
-console.log(checkTypeOf);
+//console.log(checkTypeOf);
 
 
+//method filter
+let positiveNums: number[] =[2,3,4,5,6];
+let filteredArr: number [] = positiveNums.filter((num: number) => num > 4)
+//console.log("filteedArr", filteredArr);
+
+//method sort()
+let sortNums: number[] = [100, 2 , 30, 5, 40, 6];
+let sortedResult = sortNums.sort((a: number, b: number) => a-b);
+//console.log(sortedResult);
 
 
+//method push() - add element 
+sortedResult.push(200);
+//console.log(sortedResult);
+
+//method pop() - remove element from the last 
+sortedResult.pop();
+//console.log(sortedResult);
+
+//method shift() - remove first element (0 index)
+sortedResult.shift();
+//console.log(sortedResult);
+
+//method unshift() - add first element 
+sortedResult.unshift(1);
+console.log(sortedResult);
